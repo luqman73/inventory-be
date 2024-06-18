a55a04a22ad41836f378041199e94e6343b243e0
luqman73 <luqmanhazli@gmail.com>
2024-06-18 13:45:18 +0800

    Remove staffcontroller and merge into userController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,4 +13,22 @@ class UserController extends Controller
 
         return response()->json($staff);
     }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+            'role' => 'staff'
+        ]);
+
+        return response()->json(['message' => 'Staff registered succesfully!']);
+    }
 }
